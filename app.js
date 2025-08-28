f9c4ed1a9191247d4d8caeb34c00afbdfbf72d3e
Ousama Abou Khalil <131563820+OusamaAbouKhalil@users.noreply.github.com>
2025-08-28 22:44:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function () {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
-    });    //end of forEach() loop.
-})();
+            [...document.querySelectorAll(".control")].forEach(button => {
+                button.addEventListener("click", function () {
+                    document.querySelector(".active-btn").classList.remove("active-btn");
+                    this.classList.add("active-btn");
+                    document.querySelector(".active").classList.remove("active");
+                    document.getElementById(button.dataset.id).classList.add("active");
+                })
+            });
+        })();
